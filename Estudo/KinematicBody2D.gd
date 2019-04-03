3551df7eb0aa9bb782bb42db5f7260d4fdb5cb0f
GuilhermeGraeff <guilherme.rafael.graeff@gmail.com>
2019-04-03 16:45:54 -0300

    Atualiza Plataforma

--- Estudo/KinematicBody2D.gd
+++ Estudo/KinematicBody2D.gd
@@ -1,18 +1,22 @@
 extends KinematicBody2D	
-var motion= Vector2()
+var motion = Vector2()
 var vel=100
 var jump = 600
+var Aceleracao = 50
+var Max_Velocidade =200
 var gravidade = 20
 const UP = Vector2(0,-1)
 
 func _physics_process(delta):
 	motion.y += gravidade
-	if Input.is_action_pressed("ui_right"):
-		motion.x= vel
+	var friccao = false
+	if Input.is_action_pressed("ui_right"):		
+		motion.x = min(motion.x+Aceleracao , Max_Velocidade)
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x= -vel
+		motion.x-= Aceleracao
+		motion.x = max(motion.x-Aceleracao,-Max_Velocidade)
 		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.play("Run")
 	#elif Input.is_action_pressed("ui_up"):
@@ -20,14 +24,23 @@ func _physics_process(delta):
 	#elif Input.is_action_pressed("ui_down"):
 	#	motion.y= vel
 	else:
-		motion.x=0
+		
 		$AnimatedSprite.play("Idle")
+		friccao = true
+		
 	#	motion.y=0	
 	if is_on_floor():
+		
 		if Input.is_action_pressed("ui_up"):
 			motion.y= -jump
 			$AnimatedSprite.play("Jump")
+		if friccao == true:
+			motion.x = lerp(motion.x,0,0.2)
 	else:
 		$AnimatedSprite.play("Jump")
+		if friccao == true:
+			motion.x = lerp(motion.x,0,0.2)
 	motion = move_and_slide(motion,UP)
+	print("x ",motion.x," y ",motion.y)
+	
 	pass
